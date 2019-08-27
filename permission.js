a60eea2b67a8d082fc83fa13fc643abc40ee44f7
aicoder <malun666@126.com>
2019-08-27 17:03:38 +0800

    first commit :rocket:

---
+++ permission.js
@@ -0,0 +1,90 @@
+const Mock = require('mockjs');
+let data = Mock.mock({
+  "permission": [],
+  role: [{
+    id: 5,
+    pid: 0,
+    name: '超级管理员',
+    des: '超级管理员',
+    subon: '2019-05-08 16:54:26',
+    status: 0,
+    del: 0
+  },{
+    id: 1,
+    pid: 0,
+    name: '管理员',
+    des: '管理员',
+    subon: '2019-05-08 16:54:26',
+    status: 0,
+    del: 0
+  },{
+    id: 2,
+    pid: 0,
+    name: '后台开发员',
+    des: '后台开发员',
+    subon: '2019-05-08 16:54:26',
+    status: 0,
+    del: 0
+  },{
+    id: 3,
+    pid: 0,
+    name: '教师',
+    des: '教师',
+    subon: '2019-05-08 16:54:26',
+    status: 0,
+    del: 0
+  },{
+    id: 4,
+    pid: 0,
+    name: '学生',
+    des: '学生',
+    subon: '2019-05-08 16:54:26',
+    status: 0,
+    del: 0
+  }],
+  user_role: [],
+  user_permission: [
+  ],
+  "role_permission": [],
+  menu: [{
+    id: 1,
+    order: 1,
+    title: '学生管理',
+    des: '学生管理',
+    url: '/student',
+    del: 0,
+    status: 0,
+    pid: 0,
+    permissionId: 1
+  }, {
+    id: 2,
+    order: 2,
+    title: '教师管理',
+    des: '教师管理',
+    url: '/teacher',
+    del: 0,
+    status: 0,
+    pid: 0,
+    permissionId: 2
+  },{
+    id: 3,
+    order: 3,
+    title: '学生列表',
+    des: '学生列表',
+    url: '/student/list',
+    del: 0,
+    status: 0,
+    pid: 1,
+    permissionId: 3
+  }, ],
+  resource:[{
+    id: 1,
+    pid: 0,
+    del: 0,
+    status: 0,
+    type: 'button',
+    permissionCode: 6,
+    permissionId: 6
+  }]
+});
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,109 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const permissionData = require('./permission');
+const captcha = require('svg-captcha');
+const perRouter = jsonServer.router(permissionData);
+const userArr = require('./user');
+const routerUser = jsonServer.router({user:userArr});
+const multer = require('multer');
+const path = require('path');
+const middlewares = jsonServer.defaults();
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, 'public/server/upload')); // 文件存储的路径
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname); // 调整文件的保存地址
+  }
+});
+const upload = multer({ storage: storage });
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+// 所有的api的请求都要求登陆后才能获取到对应的数据
+server.use('/per', (req, res, next) => {
+  if (req.get('Authorization')) {
+    next();
+  } else {
+    res.status(401).jsonp({
+      code: 8,
+      msg: '用户没有登录，不能访问'
+    });
+  }
+});
+
+// 用户登录校验
+server.post('/api/userlogin', (req, res) => {
+  let userName = req.body.username;
+  let password = req.body.password;
+  let loginUser = userArr.find((item) => item.username == userName && item.password == password);
+  if(loginUser) {
+    res.jsonp({
+      user: loginUser,
+      code: 1,
+      msg: '授权成功',
+      token: 'aicoder.com ' + Date.now()
+    });
+  } 
+  else {
+    res.jsonp({
+      code: 0,
+      msg: '用户名或者密码错误'
+    });
+  }
+});
+
+// 验证码
+server.get('/per/code', (req,res)=>{
+  const cap = captcha.create();
+  // req.session.captcha = cap.text; // session 存储
+  res.type('svg'); // 响应的类型
+  res.send(cap.data);
+});
+// 文件上传
+server.all('/per/upload', upload.single('imgF'), function(req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  var file = req.file;
+  console.log(file);
+
+  console.log('文件类型：%s', file.mimetype);
+  console.log('原始文件名：%s', file.originalname);
+  console.log('文件大小：%s', file.size);
+  console.log('文件保存路径：%s', file.path);
+  res.json({ img: `/server/upload/${file.filename}` });
+});
+
+// 权限相关接口
+// 获取用户的所有权限
+server.use('/per/getUserPer/:id', (req, res) => {
+  // 获得当前用户的特殊权限
+  let userPerArr = permissionData.user_permission.filter(item => item.userId == req.params.id);
+  let userPerIdArr = userPerArr.map(userPer => userPer.permissionId);
+  // 获取用户的角色
+  let userRoleArr = permissionData.user_role.filter(item => item.userId == req.params.id);
+  // 获得所有角色的权限
+  let rolePerIdArr = [];
+  permissionData.role_permission.forEach( rolePer => {
+    if(userRoleArr.find(userRole => userRole.roleId == rolePer.roleId)) {
+      rolePerIdArr.push(rolePer.permissionId);
+    }
+  });
+  
+  let totalPerIdArr =[...new Set([...userPerIdArr, ...rolePerIdArr])];
+  let result = [];
+  
+  // 把所有权限
+  permissionData.permission.forEach(per => {
+    if(totalPerIdArr.find(perId => per.id == perId && per.del == 0)) {
+      result.push(per);
+    }
+  });
+  res.json(result);
+});
+server.use('/per', perRouter);
+server.use('/per/', routerUser);
+
+server.listen(8888, () => {
+  console.log('JSON Server is running, http://localhost:8888');
+});
