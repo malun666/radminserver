847c51d24bcfc52008ed0619c899ca740fd1743c
aicoder <malun666@126.com>
2019-09-27 16:03:54 +0800

    update the user phone number

--- user.js
+++ user.js
@@ -48,4 +48,52 @@ module.exports =  [{
   mail: Random.email(),
   phone: '189222222',
   isTeacher: true
+}, {
+  id: 1003,
+  username: '18911114444',
+  password: 'aicoder.com',
+  del: false,
+  active: true, //  激活
+  avatar: 'http://192.168.1.130:8888/server/img/a1.png',
+  name: "李思",
+  school: '清华大学',
+  mail: Random.email(),
+  phone: '189222222',
+  isTeacher: true
+}, {
+  id: 1004,
+  username: 'admin1',
+  password: 'aicoder.com',
+  del: false,
+  active: true, //  激活
+  avatar: 'http://192.168.1.130:8888/server/img/a1.png',
+  name: "李思",
+  school: '清华大学',
+  mail: Random.email(),
+  phone: '189222222',
+  isTeacher: true
+}, {
+  id: 1005,
+  username: '18911113333',
+  password: 'aicoder.com',
+  del: false,
+  active: true, //  激活
+  avatar: 'http://192.168.1.130:8888/server/img/a1.png',
+  name: "李思",
+  school: '清华大学',
+  mail: Random.email(),
+  phone: '189222222',
+  isTeacher: true
+}, {
+  id: 1006,
+  username: '18911112222',
+  password: 'aicoder.com',
+  del: false,
+  active: true, //  激活
+  avatar: 'http://192.168.1.130:8888/server/img/a1.png',
+  name: "李思",
+  school: '清华大学',
+  mail: Random.email(),
+  phone: '189222222',
+  isTeacher: true
 }]
